dab9cc63afc104a621c9fc3a580ded0c10e5aa46
acnk <2479303241@qq.com>
2026-03-11 21:36:24 +0800

    Update full-stack blog project

--- push-blog.bat
+++ push-blog.bat
@@ -1,9 +1,4 @@
 @echo off
-echo Starting Spring Boot backend...
-start cmd /c "cd D:\project\my_blog\my_blog && mvn spring-boot:run"
-
-echo Starting Vue frontend...
-start cmd /c "cd D:\project\my_blog\my_blog_frontend && npm run dev"
-
-echo Both servers started!
+cd /d "D:\project\my_blog"
+powershell -ExecutionPolicy Bypass -File "git-push-blog.ps1"
 pause
